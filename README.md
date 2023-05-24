17d86cb2bdda644daaf83323a5fd69c53043f71d
mrmao <wwu710632@gmail.com>
2023-05-24 14:07:44 +0800

    feat: init commit

--- README.md
+++ README.md
@@ -1,15 +1,66 @@
-# pkg-name
+# vite-plugin-logs
 
-[![NPM version](https://img.shields.io/npm/v/pkg-name?color=a1b858&label=)](https://www.npmjs.com/package/pkg-name)
+[![NPM version](https://img.shields.io/npm/v/vite-plugin-logs?color=a1b858&label=)](https://www.npmjs.com/package/pkg-name)
 
-## Sponsors
+A vite plugin for html inject logs
 
-<p align="center">
-  <a href="https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg">
-    <img src='https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg'/>
-  </a>
-</p>
+## Install
+
+```bash
+
+npm install vite-plugin-logs D
+
+```
+
+## Usage
+
+```ts
+import { defineConfig } from 'vite'
+
+import logs, { preset } from 'vite-plugin-logs'
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      logs(
+        preset([
+          { type: 'darkblue:gray', label: '..', value: '{_name_}' },
+          { type: 'darkblue:green', label: '..', value: mode },
+          { type: 'darkblue:blue', label: '..', value: '{_version_}' },
+          { type: 'darkblue:blue', label: '..', value: '{_time_}' },
+        ])
+      ),
+    ]
+  }
+})
+```
+
+<img height="200" src="public/1.png" />
+
+## Custom
+  
+```ts
+import { defineConfig } from 'vite'
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      logs([
+        [
+          { text: 'Project', style: {/* ... */} },
+          { text: ' {_NAME_} ', style: {/* ... */} },
+        ],
+        [
+          { text: 'Environment', style: {/* ... */} },
+          { text: ` ${mode} `, style: {/* ... */} },
+        ],
+        // ....
+      ]),
+    ]
+  }
+})
+```
 
 ## License
 
-[MIT](./LICENSE) License © 2022 [Anthony Fu](https://github.com/antfu)
+[MIT](./LICENSE) License © 2023 [Hairyf](https://github.com/hairyf)
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,96 @@
-export const one = 1
-export const two = 2
+import path from 'node:path'
+import fs from 'fs-extra'
+import type { Plugin } from 'vite'
+import type { CSSProperties } from '@vue/runtime-dom'
+
+export interface Options {
+  text: string
+  style?: CSSProperties
+}
+
+// vite-plugin-logs
+function logs(logs: Options[][] = []): Plugin {
+  return {
+    name: 'vite:project-logs',
+    enforce: 'pre',
+    transformIndexHtml(html) {
+      const js = `<script>${generate(logs)}</script>`
+      return html.replace(/<head>/i, `<head>\n${js}`)
+    },
+  }
+}
+
+function generate(logs: Options[][]) {
+  const packageJSON = fs.readJSONSync(path.resolve('package.json'))
+  const prints = logs.map((log) => {
+    return {
+      text: `%c ${log.map(log => replace(log.text)).join(' %c')}`,
+      style: log.map(log => varcss(log.style)),
+    }
+  })
+
+  function replace(text: string) {
+    return text
+      .replace('{_NAME_}', packageJSON.name)
+      .replace('{_VERSION_}', packageJSON.version)
+      .replace('{_TIME_}', new Date().toLocaleString())
+  }
+
+  return `${JSON.stringify(prints)}.forEach(log => console.log(log.text, ...log.style));`
+}
+
+function varcss(style: CSSProperties = {}) {
+  return Object
+    .entries(style)
+    .map(([key, value]) => `${key}: ${value}`)
+    .join(';')
+}
+
+const styles: Record<string, CSSProperties> = {
+  darkblue: {
+    'padding': '2px 1px',
+    'color': '#fff',
+    'border-radius': '3px 0 0 3px',
+    'background': '#35495e',
+  },
+  blue: {
+    'padding': '2px 1px',
+    'color': '#fff',
+    'border-radius': '0 3px 3px 0',
+    'background': '#2a61c4',
+  },
+  gray: {
+    'padding': '2px 1px',
+    'color': '#fff',
+    'border-radius': '0 3px 3px 0',
+    'background': '#9f9f9f',
+  },
+  green: {
+    'padding': '2px 1px',
+    'color': '#fff',
+    'border-radius': '0 3px 3px 0',
+    'background': '#89bf04',
+  },
+}
+
+interface PresetOptions {
+  type: 'darkblue:gray' | 'darkblue:green' | 'darkblue:blue'
+  label: string
+  value: string
+}
+
+export function preset(options: PresetOptions[]) {
+  const logs: Options[][] = []
+  for (const option of options) {
+    const [label, value] = option.type.split(':')
+    logs.push(
+      [
+        { text: option.label, style: styles[label] },
+        { text: option.value, style: styles[value] },
+      ],
+    )
+  }
+  return logs
+}
+
+export default logs
